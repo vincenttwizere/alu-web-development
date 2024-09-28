80f4696a70e2c1257ec2ea5ac130c7c34af7de11
Vincent TWIZERE <116659480+vincenttwizere@users.noreply.github.com>
2024-09-29 00:25:00 +0300

    Update user.py

--- user_authentication_service/user.py
+++ user_authentication_service/user.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python3
-# Archemy User class
+"""
+Module that defines the User class for SQLAlchemy ORM.
+
+This module contains the User class, which represents a user
+in the database with fields for id, email, hashed_password,
+session_id, and reset_token. It is used to manage user-related
+data in the system.
+"""
 
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
@@ -7,7 +14,7 @@ from sqlalchemy import Column, Integer, String
 Base = declarative_base()
 
 class User(Base):
-    """User class"""
+    """User class represents a user record in the database."""
     __tablename__ = "users"
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
